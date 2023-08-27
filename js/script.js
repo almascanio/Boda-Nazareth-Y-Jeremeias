48a8cf6bf7b124ce0078e1777ce443cd0f0d772e
almascanio <almascanio08@gmail.com>
2023-08-26 22:58:39 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function actualizarContador() {
+  const fechaObjetivo = new Date('2023-11-11T14:00:00'); // Fecha y hora objetivo
+  const ahora = new Date(); // Hora actual
+
+  const diferencia = fechaObjetivo - ahora; // Diferencia en milisegundos
+  const segundos = Math.floor(diferencia / 1000);
+  const minutos = Math.floor(segundos / 60);
+  const horas = Math.floor(minutos / 60);
+  const dias = Math.floor(horas / 24);
+
+  const segundosRestantes = segundos % 60;
+  const minutosRestantes = minutos % 60;
+  const horasRestantes = horas % 24;
+
+  document.getElementById('dias').textContent = dias.toString().padStart(2, '0');
+  document.getElementById('horas').textContent = horasRestantes.toString().padStart(2, '0');
+  document.getElementById('minutos').textContent = minutosRestantes.toString().padStart(2, '0');
+  document.getElementById('segundos').textContent = segundosRestantes.toString().padStart(2, '0');
+}
+
+actualizarContador(); // Actualizar contador inmediatamente
+setInterval(actualizarContador, 1000); // Actualizar cada segundo
+
+/* Menu hamburguesa */
+
+const menuListas = document.querySelectorAll(".menu-li");
+const menuButton = document.querySelector("#menu-button");
+
+menuListas.forEach(function(lista){
+  lista.addEventListener("click", function(e){
+      const activeLista = document.querySelector(".active");
+      activeLista.classList.remove("active");
+      e.target.classList.add("active");
+  })
+});
+
+menuButton.addEventListener("click", function(){
+  document.body.classList.toggle("menu-mobile")
+});
+
+
+const reproductor = document.getElementById("reproductor");
+const audio = document.getElementById("miAudio");
+let reproduciendo = false;
+
+reproductor.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (reproduciendo) {
+        audio.pause();
+        reproduciendo = false;
+        reproductor.querySelector("i").classList.remove("bi-pause-fill");
+        reproductor.querySelector("i").classList.add("bi-caret-right-fill");
+    } else {
+        audio.play();
+        reproduciendo = true;
+        reproductor.querySelector("i").classList.remove("bi-caret-right-fill");
+        reproductor.querySelector("i").classList.add("bi-pause-fill");
+    }
+});
